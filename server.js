12903b3e0dd61cd315c5375b594cfc1de9bbc1c9
Patrick Machado <paamachado.93@gmail.com>
2021-06-03 15:45:55 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+{
+  "name": "test-server",
+  "version": "1.0.0",
+  "description": "A http server that returns the request headers",
+  "author": "Patrick Machado <paamachado.93@gmail.com>",
+  "license": "ISC",
+  "main": "server.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1"
+  }
+} 
